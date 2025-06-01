19efae24f7c89a598e0dc5774ebb91b1d20a760c
tifanymeunier <tifany.meunier@etu.univ-orleans.fr>
2025-06-01 15:19:29 +0200

    Delete GraphProject directory

--- GraphProject/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Compilation..."
-mkdir -p bin
-javac -cp "lib/*" -d bin src/graph/*.java src/json/*.java src/pdf/*.java src/Main.java
-
-echo "Execution..."
-java -cp "bin:lib/*" Main
